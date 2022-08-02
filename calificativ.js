bf38c02eaaa3375fa3610a9a432521fb1336af87
Cornel <cornel.cibotaru@itschool.ro>
2022-08-02 08:07:09 +0300

    Exercițiu calificative

---
+++ calificativ.js
@@ -0,0 +1,16 @@
+/*
+    Scrie o funcție care primește ca parametru un număr și întoarce un calificativ.
+    Încearcă să scrii cât mai puține instrucțiuni.
+    
+    Calificative:
+    10: FB
+     9: FB
+     8: B
+     7: B
+     6: S
+     5: S
+     4: IS
+     3: IS
+     2: IS
+     1: IS
+*/
