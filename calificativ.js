9a86af62cc502a196444c30940c0f80dab2a1dc5
Alexandra Toma <tomaalexandra8@gmail.com>
2022-08-05 17:34:18 +0300

    temarefacuta

--- calificativ.js
+++ calificativ.js
@@ -15,21 +15,21 @@
      1: IS
 */
 const nume = function calificative() {
-    const note = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let nota = 10;
+    let note=[1,2,3,4,5,6,7,8,9,10];
     let cal;
-  
-    for (let i = 0; i < note.length; i++) {
-      if (nota >= 9) {
+     for(let i=0;i<note.length;i++){
+       note[i]=7;
+      if (note[i]>8 && note[i]<=10) {
         cal = "FB";
-      } else if (nota >= 7) {
+      } else if (note[i]>6 && note[i]<=8 ) {
         cal = "B";
-      } else if (nota >= 5) {
+      } else if (note[i]>4 && note[i]<=6) {
         cal = "S";
-      } else if (nota <= 4) {
+      } else if (note[i] <= 4 && note[i]>0) {
         cal = "IS";
-      }
-      console.log(nota + ":" + cal);
+      } 
+      console.log(note[i] + ":" + cal);
     }
-  };
+  }
+  
   nume();
