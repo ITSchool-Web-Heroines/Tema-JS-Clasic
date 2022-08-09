37253ccc9fc62a64bf267d6ee3fa960b55279877
AliceIoana <aliceioana1007@gmail.com>
2022-08-09 09:26:42 +0300

    return added

--- calificativ.js
+++ calificativ.js
@@ -1,12 +1,12 @@
 const notes = function (mark) {
   if (mark == 10 || mark == 9) {
-    console.log("FB");
+    return console.log("FB");
   } else if (mark == 8 || mark == 7) {
-    console.log("B");
+    return console.log("B");
   } else if (mark == 6 || mark == 5) {
-    console.log("S");
+    return console.log("S");
   } else if (mark == 4 || mark == 3 || mark == 2 || mark == 1) {
-    console.log("IS");
+    return console.log("IS");
   }
 };
 
