3a36242a5874dfa1517e657c758e4358d04f4d6e
AliceIoana <aliceioana1007@gmail.com>
2022-08-12 10:32:58 +0300

    last modifications

--- calificativ.js
+++ calificativ.js
@@ -1,13 +1,14 @@
 const notes = function (mark) {
-  if (mark == 10 || mark == 9) {
-    return console.log("FB");
-  } else if (mark == 8 || mark == 7) {
-    return console.log("B");
-  } else if (mark == 6 || mark == 5) {
-    return console.log("S");
-  } else if (mark == 4 || mark == 3 || mark == 2 || mark == 1) {
-    return console.log("IS");
+  if (mark >= 9) {
+    return "FB";
   }
+  if (mark >= 7) {
+    return "B";
+  }
+  if (mark >= 5) {
+    return "S";
+  }
+  return "IS";
 };
 
 notes(10);
---
+++ miorita.js
@@ -0,0 +1,21 @@
+/*
+    Replicați funcționalitatea Mioriței, folosind obiecte.
+    Miorița se află într-o stână infinită.
+*/
+
+const miorita = {
+  x: 0,
+  y: 0,
+  move: function (moveX, moveY) {
+    this.x = this.x + moveX;
+    this.y = this.y + moveY;
+  },
+};
+
+miorita.move(1, 3);
+console.log(miorita.x, miorita.y);
+// 1 3
+
+miorita.move(1, -4);
+console.log(miorita.x, miorita.y);
+// 2 -1
